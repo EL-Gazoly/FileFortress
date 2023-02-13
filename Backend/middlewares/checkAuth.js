288fab59ece018fda0f4cac041f851de16ef6557
Adham ElGazoly <elgazoly11@gmail.com>
2023-02-14 00:00:01 +0200

    checking user authentication function

---
+++ Backend/middlewares/checkAuth.js
@@ -0,0 +1,9 @@
+module.exports = isAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      console.log("good to go ");
+      next();
+    } else {
+      res.status(401).json({ msg: "Please Login" });
+    }
+  };
+  
