3f43d523fe4456d721019e46f0e32d2e07fb744d
Adham ElGazoly <elgazoly11@gmail.com>
2023-02-13 19:03:19 +0200

    added register function

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const passport = require('passport')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 const app = express()
 
+//middelwares 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Application routes
 app.use('/api/upload/', require('./routes/uploadRouter'))
+app.use('/api/user/', require('./routes/UserRoutes'))
 
 
 
