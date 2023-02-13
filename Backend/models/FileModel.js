02da755fc485dbea08b385b5eb5ae30d32583378
Adham ElGazoly <elgazoly11@gmail.com>
2023-02-14 00:02:37 +0200

    Link file to user with updating the user usage

--- Backend/models/FileModel.js
+++ Backend/models/FileModel.js
@@ -1,6 +1,6 @@
 const {DataTypes , UUIDV4} = require('sequelize')
 const sequelize = require('../config/database')
-
+const {unlink} = require('fs')
 const File = sequelize.define(
     "File",
     {
@@ -35,6 +35,43 @@ const File = sequelize.define(
         }
     }
 )
+File.afterCreate(async (filedata, options) => {
+    const User = require('./UserModel')
+    
+    User.findOne({
+        where : {id: filedata.ownerId}
+    })
+    .then((user)=>{
+        if (user) {
+            user.usage = user.usage + Number(filedata.size)
+            user.save()
+            console.log(user.usage)
+        }
+    })
+    .catch((err)=> {
+        console.log(err)
+    })
+})
+File.afterDestroy(async (filedata, options) => {
+    const User = require('./UserModel')
+    
+    User.findOne({ where: {id : filedata.ownerId}})
+    .then((user) => {
+        if(user) {
+            user.usage = user.usage - filedata.size
+            user.save()
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+    unlink(filedata.path, (err) => {
+        if (err) throw err
+        console.log('file deleted successfully')
+    })    
+})
+
+
 File.sync()
 
 module.exports = File
