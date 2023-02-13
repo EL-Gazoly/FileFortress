a10fbad7062e7f8d3db9b49db649d0dc1a364b9f
Adham ElGazoly <elgazoly11@gmail.com>
2023-02-14 00:05:39 +0200

    add passport configuration

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const passport = require('passport')
-const bodyParser = require('body-parser')
+const session = require('./config/session')
 require('dotenv').config()
-
+const passportInitialize = require('./config/passport')
 const app = express()
 
 //middelwares 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+
+passportInitialize()
+
+app.use(session)
+app.use(passport.initialize())
+app.use(passport.session())
 
 //Application routes
 app.use('/api/upload/', require('./routes/uploadRouter'))
