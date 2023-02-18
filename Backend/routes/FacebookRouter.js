3df243e8671a23aa58d33bb3c6b148a9e8185ee8
Adham ElGazoly <elgazoly11@gmail.com>
2023-02-18 16:46:11 +0200

    removed unnecessary scope

--- Backend/routes/FacebookRouter.js
+++ Backend/routes/FacebookRouter.js
@@ -2,7 +2,7 @@ const FacebookRouter = require('express').Router()
 const passport = require('passport')
 
 
-FacebookRouter.get('/', passport.authenticate('facebook', {scope: ['email']}))
+FacebookRouter.get('/', passport.authenticate('facebook'))
 
 FacebookRouter.get('/callback', passport.authenticate('facebook', {failureRedirect: '/login'}),
 function(req, res){
