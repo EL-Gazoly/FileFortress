00462c271fba52e444c5d9a31e8cb6e22279fb21
Adham ElGazoly <elgazoly11@gmail.com>
2023-03-01 22:18:16 +0200

    add the footer and the progress bar

---
+++ frontend/src/components/ProgressBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+const ProgressBar = ({used}) => {
+    const containerStyles = {
+        height: '12px',
+        width: '100%',
+        backgroundColor: "#6672FB",
+        borderRadius: 50,
+        
+      }
+      const fillerStyles = {
+        height: '100%',
+        width: `${used}%`,
+        backgroundColor: "#F8B849",
+        borderRadius: 'inherit',
+      }
+      const labelStyles = {
+        padding: 5,
+        color: 'white',
+        fontWeight: 'bold'
+      }
+    
+
+  return (
+    <div style={containerStyles}>
+        <div style={fillerStyles}>
+        </div>
+    </div>  
+  );
+}
+
+export default ProgressBar
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -6,6 +6,8 @@ import MenuItem from '../../components/MenuItem'
 import Logout from '../../assets/logoutLogo.svg'
 import cloudLogo from '../../assets/cloudLogo.svg'
 import Starred from '../../assets/startredLogo.svg'
+import ProgressBar from '../../components/ProgressBar'
+import whiteCloudLogo from '../../assets/whiteCloudLogo.svg'
 
 const MenuItemList = [
   {sname: 'Fortress' , logo: cloudLogo , path: '/dashboard'},
@@ -20,7 +22,8 @@ const Dashboard = () => {
         <div className="dashboard-container">
           <div className="dashboard-left">
               <div className="dashboard-left-list">
-                <button className="logo">
+                <div className="dashboard-left-list-top">
+                     <button className="logo">
                     <img src={logo} alt="logo" />                  
                 </button>
                 <div className="menu-list">
@@ -30,6 +33,23 @@ const Dashboard = () => {
                     <MenuItem name={item.sname} logo={item.logo}  path={item.path} />
                     </div>
                   ))} 
+                </div>
+                </div>
+             
+                <div className="dashboard-left-footer">
+                  <div className="dashboard-left-footer-firs-row">
+                    <div>
+                      <img src={whiteCloudLogo} alt="logo" />
+                      <span>Storage</span>
+                    </div>
+                      <p>27%</p>                    
+                  </div>
+                  <div className="dashboard-left-footer-second-row">
+                    <ProgressBar used={27} /> 
+                  </div>
+                  <div className='dashboard-left-footer-third-row'>
+                    27/100 GB Used
+                  </div>
                 </div>
               </div>
           </div>
