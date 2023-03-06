a9c3f17b31e2d9aad6b8f2ae6c29de584d774b0f
Adham ElGazoly <elgazoly11@gmail.com>
2023-03-06 22:07:52 +0200

    added the icons and the first 3 rows to signup page

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,75 @@
 import './SignUp.css'
 import React from 'react'
+import signUpIcon from '../../assets/signUpIcon.svg'
+import nameInputFieldIcon from '../../assets/nameInputFieldIcon.svg'
+import Header from '../../components/Header'
 
 const SignUp = () => {
   return (  
-    <div className='signUp-container'>  
+    <div className='container'>  
+      <div className="signup-page-container">
+            <Header />
+            <div className="signup-page-body">
+                <div className="signup-page-body-left">
+                    <img src={signUpIcon} alt="Sign Up Icon" />
+                </div>
+                <div className="signup-page-body-right">
+                      <div className="signup-page-body-right-firt-row">
+                        <h1>Sign Up</h1>
+                      </div>
+                      <div className="signup-page-body-right-second-row">
+                        <p>Fill in your credentials and click on the the Sign up button</p>
+                      </div>
+                      <div className="signup-page-body-right-third-row">
+                        <div className="first-name-input">
+                          <label htmlFor="name">
+                            First name
+                          </label>
+                          <div className="first-name-input-field">
+                            <input type="text" name="name" id="name" placeholder='Your first name'/>
+                            <img src={nameInputFieldIcon} alt="name-input-field-icon" />
+                          </div>
+                          
+                        </div>
+                        <div className="last-name-input">
+                          <label htmlFor="name">
+                            Last name
+                          </label>
+                          <div className="last-name-input-field">
+                            <input type="text" name="name" id="name" placeholder='Your last name'/>
+                            <img src={nameInputFieldIcon} alt="name-input-field-icon" />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="signup-page-body-right-fourth-row">
+                        <label htmlFor="email"></label>
+                        <div className="email-input-field">
+                          <input type="email" name="email" id="email" />
+                        </div>
+                      </div>
+                      <div className="signup-page-body-right-fifth-row">
+                        <label htmlFor="password"></label>
+                        <div className="password-input-field">
+                          <input type="password" name="password" id="password" />
+                        </div>
+                      </div>
+                      <div className="signup-page-body-right-sixth-row">
+                        <button className="signup-button">Sign up</button>
+                      </div>
+                      <div className="signup-page-body-right-seventh-row">
+                        <p>OR</p>
+                      </div>
+                      <div className="signup-page-body-right-eighth-row">
+                        <div className="sign-up-options">
 
+                        </div>
+                      </div>
+                      <div className="signup-page-body-right-ninth-row">
+                        <p>Already have an account? <a href="#">Sign In</a></p> 
+                      </div>
+                </div>
+            </div>
+      </div>
     </div>
   )
 }
